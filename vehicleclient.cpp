f9b11678519c213d01d05dbdf248e1700b832eb2
Perulmaien <alexandra.mikovits@aon.at>
2015-05-30 12:37:48 +0200

    Version1

---
+++ vehicleclient.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <getopt.h>
+#include <unistd.h>
+//#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
