ebdec535141ccab1f49177233444d8df273984c4
Perulmaien <alexandra.mikovits@aon.at>
2015-05-30 14:39:36 +0200

    Moving Schleife implementiert

--- vehicleclient.cpp
+++ vehicleclient.cpp
@@ -7,19 +7,45 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if(argc < 2)
+        if(argc < 2) //Wenn kein Wagenname eingegeben wurde
     {
         return -1;
     }
 
-
     char name = argv[1][0];
     char moving;
 
+
+
+    if (name <'A' || name > 'Z') //Wenn der Wagenname kein Großbuchstabe war
+    {
+        return -1;
+    }
+
     cout << "Registration OK" << endl;
     cout << name << endl;
-    cout << "Enter Move: ";
-    cin >> moving;
-    cout << "The input was" << moving << endl;
+    //while(/*Auto nicht geschrottet */)
+    //{
+        cout << "Enter Move: ";
+        cin >> moving;
+
+        switch(moving)
+        {
+            case 'N' : //Queue schicken mit dem Move
+            case 'O' :
+            case 'S' :
+            case 'W' :
+            case 'T' :     cout << "The input was " << moving << endl;
+                    break;
+            default: cout << "Ungültiger Move" << endl;
+                    break;
+        }
+
+        // ?? Überprüfung ob Auto geschrottet oder nicht
+  //  }
+
+    cout << "Vehicle has been eliminated." << endl;
+
+
     return 0;
 }
