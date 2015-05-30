570e87153507f5918afedd86db59b5d7d02a209e
litterboks <if14b049@technikum-wien.at>
2015-05-30 18:40:17 +0200

    Display Client läuft

--- Display/griddisplay.cpp
+++ Display/griddisplay.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
 #include <unistd.h>
 #include <signal.h>
-//#include <stdlib.h>
-#include <griddisplay.h>
+#include <fstream>
+#include <sys/msg.h>
+#include <stdlib.h>
+#include "griddisplay.h"
 
 void signal_handler(int sig)
 {
-    if(sig == SIGINT)
-    {
-        exit(EXIT_SUCCESS);
-    }
+	if (sig == SIGINT) {
+		exit(EXIT_SUCCESS);
+	}
 }
 
 
 using namespace std;
 
-void Grid::Griddisplay ()
+int Display::Griddisplay()
+{
+	std::ifstream f;
+	f.open("../pipe");
+
+	if (f.is_open() != true) {
+		cout << "Error with Signal" << endl;
+		return -1 ;
+	}
+	cout << f << endl;
+	return 0;
+}
 
 
 int main()
 {
-    int height = 0;
-    int width = 0;
-(void) signal(SIGINT, signal_handler);
+	Display d;
+	(void) signal(SIGINT, signal_handler);
 
-    while (1==1)
-    {
-        Grid::Griddisplay();
-
-    }
-
-
-
-    return 0;
-}
-
-void Grid::Griddisplay ()
-{
-   ifstream f;
-   f.open("../pipe");
-
-    if(f.is_open() !=true)
-    {
-        cout << "Error with Signal" << endl;
-        return -1;
-    }
-    cout << f << endl;
+	while (1 == 1) {
+		int x = d.Griddisplay();
+		if (x == -1) {
+			return -1;
+		}
+	}
+	return 0;
 }
